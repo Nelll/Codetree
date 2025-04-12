465720cf8781f3117ae24c87b199ef43e4e4e915
TA_Kuma <fj3983@naver.com>
2025-04-13 02:12:11 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250413/중앙값 구하기/find-the-median.cpp
+++ 250413/중앙값 구하기/find-the-median.cpp
@@ -8,23 +8,38 @@ int main() {
 
     if(a > b)
     {
-        if(a < c)
-        {
-            cout << a;
-        }
-        else if( a > c && b < c)
-        {
-            cout << c;
-        }
-    }else
-    {
-        if(b < c)
+        if(b > c)
         {
             cout << b;
         }
-        else if(b > c && a < c)
+        else
         {
-            cout << c;
+            if(a > c)
+            {
+                cout << c;
+            }
+            else
+            {
+                cout << a;
+            }
+        }
+    }
+    else
+    {
+        if(a > c)
+        {
+            cout << a;
+        }
+        else
+        {
+            if(b > c)
+            {
+                cout << c;
+            }
+            else
+            {
+                cout << b;
+            }
         }
     }
     return 0;
